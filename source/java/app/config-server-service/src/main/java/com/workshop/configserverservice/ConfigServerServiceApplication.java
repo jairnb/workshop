c2741388677c445e9af30cfa55b9bc33ef9547e4
jairnb <jair10.nb@hotmail.com>
2022-07-22 22:12:52 -0500

    profiles configurations

--- source/java/app/config-server-service/src/main/java/com/workshop/configserverservice/ConfigServerServiceApplication.java
+++ source/java/app/config-server-service/src/main/java/com/workshop/configserverservice/ConfigServerServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+
 public class ConfigServerServiceApplication {
 
     public static void main(String[] args) {
