aeb67a751faaeab24781732853ce515829a53cd1
Pedro Fernandes <jair10.nb@hotmail.com>
2023-11-15 22:29:05 -0500

    intellij error

--- source/java/workshop/config-server-service/src/main/java/com/workshop/configserverservice/ConfigServerServiceApplication.java
+++ source/java/workshop/config-server-service/src/main/java/com/workshop/configserverservice/ConfigServerServiceApplication.java
@@ -2,8 +2,10 @@ package com.workshop.configserverservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerServiceApplication {
 
 	public static void main(String[] args) {
