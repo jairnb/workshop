b82b7ab91128094ef7ab5caac0896f7bad72a470
Pedro Fernandes <jair10.nb@hotmail.com>
2023-11-17 22:55:10 -0500

    employee-service initial work

--- source/java/workshop/employee-service/src/main/java/com/workshop/employeeservice/EmployeeServiceApplication.java
+++ source/java/workshop/employee-service/src/main/java/com/workshop/employeeservice/EmployeeServiceApplication.java
@@ -2,6 +2,9 @@ package com.workshop.employeeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class EmployeeServiceApplication {
@@ -10,4 +13,14 @@ public class EmployeeServiceApplication {
         SpringApplication.run(EmployeeServiceApplication.class, args);
     }
 
+}
+
+@RequestMapping("/employees")
+@RestController
+class Test {
+
+    @GetMapping
+    public String home() {
+        return "Home";
+    }
 }
